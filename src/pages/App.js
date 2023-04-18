85f86e027d362e6f22351f43fba876af7f2498d7
Lokfor <pedro.rodrma@gmail.com>
2023-04-18 14:56:25 -0300

    Finalizado.

--- src/pages/App.js
+++ src/pages/App.js
@@ -15,22 +15,23 @@ function App() {
 
 
   const handleSearchRepo = async () => {
-
-    const {data} = await api.get(`repos/${currentRepo}`)
-
-    if(data.id){
-
-      const isExist = repos.find(repo => repo.id === data.id);
-
-      if(!isExist){
-        setRepos(prev => [...prev, data]);
-        setCurrentRepo('')
-        return
+    try {
+      const {data} = await api.get(`users/${currentRepo}`)
+  
+      if(data.id){
+        const isExist = repos.find(repo => repo.id === data.id);
+  
+        if(!isExist){
+          setRepos(prev => [...prev, data]);
+          setCurrentRepo('')
+          return
+        }
       }
-
+  
+      alert('Repositório não encontrado')
+    } catch (error) {
+      alert('Repositório não encontrado')
     }
-    alert('Repositório não encontrado')
-
   }
 
   const handleRemoveRepo = (id) => {
@@ -45,7 +46,7 @@ function App() {
       <img src={gitLogo} width={72} height={72} alt="github logo"/>
       <Input value={currentRepo} onChange={(e) => setCurrentRepo(e.target.value)} />
       <Button onClick={handleSearchRepo}/>
-      {repos.map(repo => <ItemRepo handleRemoveRepo={handleRemoveRepo} repo={repo}/>)}
+      {repos.map(repo => <ItemRepo key={repo.id} handleRemoveRepo={handleRemoveRepo} repo={repo}/>)}
     </Container>
   );
 }
